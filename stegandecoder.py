83ceb742b49adb542ac6a63e24e6fc813310722e
AYAN-AMBESH <ambesh12k@gmail.com>
2021-06-21 21:57:38 +0530

    Changes to be committed:'To convert extracted string of binary to list of characters '

--- stegandecoder.py
+++ stegandecoder.py
@@ -14,26 +14,25 @@ for x in range(73):
     pixel=r,g,b
     
     if i<216:
-        print(r)
         if r==254:
             extracted+='0'
         elif r==255:
             extracted+='1'    
 
     if i<216:
-        
-        print(r)
         if g==254:
             extracted+='0'
         elif g==255:
             extracted+='1'    
 
     if i<216:
-
-        print(r)
         if b==254:
             extracted+='0'
         elif b==255:
             extracted+='1'    
 
-print(extracted)
+chars = []
+for i in range(len(extracted)//8):
+    byte = extracted[i*8:(i+1)*8]
+    chars.append(chr(int(''.join([str(bit) for bit in byte]), 2)))
+print(chars[2:-2])
