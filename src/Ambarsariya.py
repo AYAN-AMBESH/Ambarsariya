cba4da59d8d32b18d952d68fd1d3427e02b04b2a
AYAN-AMBESH <ambesh12k@gmail.com>
2022-04-15 18:40:04 +0530

    added files

---
+++ src/Ambarsariya.py
@@ -0,0 +1,41 @@
+import os
+import sys
+from src import stegan
+from src import stegandecoder
+
+def main():
+    colors = {
+    'error':'\033[31;1m[x] ',
+    'msg':'\033[36;1m ',
+    'success':'\033[33;1m ',
+    'white':'\033[37;1m '
+    }
+    
+    print(colors['msg'] + '''
+
+    ░█████╗░███╗░░░███╗██████╗░░█████╗░██████╗░░██████╗░█████╗░██████╗░██╗██╗░░░██╗░█████╗░
+    ██╔══██╗████╗░████║██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗██╔══██╗██║╚██╗░██╔╝██╔══██╗
+    ███████║██╔████╔██║██████╦╝███████║██████╔╝╚█████╗░███████║██████╔╝██║░╚████╔╝░███████║
+    ██╔══██║██║╚██╔╝██║██╔══██╗██╔══██║██╔══██╗░╚═══██╗██╔══██║██╔══██╗██║░░╚██╔╝░░██╔══██║
+    ██║░░██║██║░╚═╝░██║██████╦╝██║░░██║██║░░██║██████╔╝██║░░██║██║░░██║██║░░░██║░░░██║░░██║
+    ╚═╝░░╚═╝╚═╝░░░░░╚═╝╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░░╚═╝░░░╚═╝░░╚═╝
+
+    By Ayan Ambesh (ambesh12k@gmail.com)
+    ''')
+
+    if '-d' in sys.argv and '-e' in sys.argv:
+        print(colors['error'] + 'Can\'t use both -d and -e')
+        sys.exit()
+        
+    elif '-d' in sys.argv:
+        stegandecoder.stegdecode()
+        
+    elif '-e' in sys.argv:
+        stegan.stegencode()
+
+    elif '-h' in sys.argv:
+        print(colors['white'] + 'Usage:\n -e to embed\n -d to decode\n\n Example: python3 ambarsariya.py -e')
+
+    else:
+        print(colors['error'] + 'No valid option specified\nBye!')
+        sys.exit()
