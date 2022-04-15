d404cb6943611d1c1d35ded8b320ad384c1f667a
sarth <sarthakgols@gmail.com>
2022-04-15 13:13:48 +0530

    Bugs removal

--- src/stegandecoder.py
+++ src/stegandecoder.py
@@ -3,49 +3,47 @@ import numpy as np
 import os
 import sys
 
+
 def stegdecode():
-    
     colors = {
-    'error':'\033[31;1m[x] ',
-    'msg':'\033[36;1m ',
-    'success':'\033[33;1m ',
-    'white':'\033[37;1m '
+        'error': '\033[31;1m[x] ',
+        'msg': '\033[36;1m ',
+        'success': '\033[33;1m ',
+        'white': '\033[37;1m '
     }
-    
-    imagepath=input(colors['white'] + "Enter image path: ")
+
+    imagepath = input(colors['white'] + "Enter image path: ")
     if os.path.exists(imagepath):
         image = Image.open(imagepath)
     else:
         print(colors['error'] + 'File path invalid xD')
         sys.exit()
 
+    extracted = ''
 
-    extracted= ''
-
-    i=0
-    img=np.array(image)
+    i = 0
+    img = np.array(image)
     for x in range(73):
-        r,g,b = img[x,0]
-        pixel=r,g,b
-        
-        if i<216:
-            rbit=bin(r)
-            rlbit=rbit[-1]
-            extracted+=str(rlbit)   
+        r, g, b = img[x, 0]
+        pixel = r, g, b
 
-        if i<216:
-            gbit=bin(g)
-            glbit=gbit[-1]
-            extracted+=str(glbit)    
+        if i < 216:
+            rbit = bin(r)
+            rlbit = rbit[-1]
+            extracted += str(rlbit)
 
-        if i<216:
-            bbit=bin(b)
-            blbit=bbit[-1]
-            extracted+=str(blbit)    
+        if i < 216:
+            gbit = bin(g)
+            glbit = gbit[-1]
+            extracted += str(glbit)
 
+        if i < 216:
+            bbit = bin(b)
+            blbit = bbit[-1]
+            extracted += str(blbit)
 
     chars = []
-    for i in range(len(extracted)//8):
-        byte = extracted[i*8:(i+1)*8]
+    for i in range(len(extracted) // 8):
+        byte = extracted[i * 8:(i + 1) * 8]
         chars.append(chr(int(''.join([str(bit) for bit in byte]), 2)))
     print('\n' + colors['success'] + ''.join(chars[2:-2]))
